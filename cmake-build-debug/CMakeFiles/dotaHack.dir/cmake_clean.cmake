7edb0fc6dd25feb4efe8ab79e70c8ee4afdab415
kevinpook <kevinpook_zxc@Hotmail.com>
2021-04-21 17:44:14 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/dotaHack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dotaHack.dir/main.cpp.obj"
+  "dotaHack.exe"
+  "dotaHack.exe.manifest"
+  "dotaHack.pdb"
+  "libdotaHack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dotaHack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
